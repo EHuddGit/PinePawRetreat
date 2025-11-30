0beec8aded6249f06e9c5a1b37c6950f86e86709
Ethan Huddleston <e.huddofficial@gmail.com>
2025-11-30 00:37:21 +0000

    AB#3155 - Contact_Page (#4)
    Co-authored-by: ethan <e.huddoffical@gmail.com>
    Reviewed-on: https://git.websurfingstudios.com/Phase1/PetBoarding_EthanHuddleston/pulls/4
    Reviewed-by: Winston Luu <winstonluu38@gmail.com>

---
+++ WebAppTemplate/ViewModels/ContactUsSubmissionVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.ViewModels
+{
+    public class ContactUsSubmissionVM
+    {
+
+        [Required]
+        public string NameFirst { get; set; }
+        [Required]
+        public string NameLast { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
