c319b478edfe1ef800afab9bd637c680a6e02fbe
ethan <e.huddoffical@gmail.com>
2026-01-05 09:00:50 -0800

    added all files needed for vaccinations

---
+++ PinePawRetreat/Controllers/VaccinationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PinePawRetreat.Controllers
+{
+    public class VaccinationsController : Controller
+    {
+        // GET: Vaccinations
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
